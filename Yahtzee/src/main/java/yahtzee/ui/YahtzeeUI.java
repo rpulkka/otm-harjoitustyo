f1ea7b11e6f9db12eb5372619f3b1bf759f99a7b
rpulkka <dreamspace2017@yandex.ru>
2018-04-05 15:46:54 +0300

    Created JavaFX scene.

--- Yahtzee/src/main/java/yahtzee/ui/YahtzeeUI.java
+++ Yahtzee/src/main/java/yahtzee/ui/YahtzeeUI.java
@@ -1,11 +1,36 @@
 
 package yahtzee.ui;
 
-/**
- *
- * @author rpulkka
- */
+import javafx.application.Application; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.scene.Scene; 
+import javafx.scene.control.Button; 
+import javafx.scene.layout.StackPane; 
+import javafx.stage.Stage;
 
-public class YahtzeeUI {
+//author rpulkka
+
+public class YahtzeeUI extends Application {
+
+    Button button;
+    
+    public static void main(String args[]){
+        launch(args);
+    }
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Yahtzee Game");
+        button = new Button();
+        button.setText("Throw the dice!");
+        
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
     
 }

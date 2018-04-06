1fa167888e5abbf13a27c7c7421473caed5a562f
rpulkka <dreamspace2017@yandex.ru>
2018-04-06 14:18:15 +0300

    Added images to layout.

--- Yahtzee/src/main/java/yahtzee/ui/YahtzeeUI.java
+++ Yahtzee/src/main/java/yahtzee/ui/YahtzeeUI.java
@@ -1,34 +1,56 @@
 
 package yahtzee.ui;
 
+
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
 import javafx.application.Application; 
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent; 
 import javafx.event.EventHandler; 
 import javafx.scene.Scene; 
 import javafx.scene.control.Button; 
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane; 
 import javafx.stage.Stage;
+import javax.imageio.ImageIO;
+
 
 //author rpulkka
 
 public class YahtzeeUI extends Application {
 
-    Button button;
     
-    public static void main(String args[]){
+    public static void main(String[] args){
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("Yahtzee Game");
-        button = new Button();
+        
+        URL urlOne = this.getClass().getResource("/images/one.png");
+        
+        InputStream inputStream = urlOne.openStream();
+        Image one = new Image(inputStream);
+        ImageView oneView = new ImageView(one);
+        
+        Button button = new Button();
         button.setText("Throw the dice!");
         
-        StackPane layout = new StackPane();
+        GridPane layout = new GridPane();
         layout.getChildren().add(button);
+        layout.getChildren().add(oneView);
         
-        Scene scene = new Scene(layout, 300, 250);
+        Scene scene = new Scene(layout);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
